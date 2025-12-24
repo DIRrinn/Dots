de5b4628c0573f172f34ef88abc71dc198bb4992
DIRrinn <cosmologicalhorizon@yandex.ru>
2025-12-24 23:07:09 +0700

    Update welcome.sh

--- hypr/welcome.sh
+++ hypr/welcome.sh
@@ -7,14 +7,16 @@ then
     echo "You've worked hard. Have a good night"
 elif [ $time -ge 06 ] && [ $time -le 10 ]
 then
-    echo "Rise and shine, Richard."
+    echo "Your time has come."
 elif [ $time -ge 11 ] && [ $time -le 15 ]
 then
     echo "Work smart, think hard. For the glory of science."
 elif [ $time -ge 16 ] && [ $time -le 20 ]
 then
-    echo "Think twice, mister Stein."
+    echo "Think twice, fellow human"
 elif [ $time -ge 21 ] && [ $time -le 23 ]
 then
     echo "Another Sun is down."
+else
+    echo "This is over."
 fi
